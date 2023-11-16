8b787abb73d84be57a44807eec7b31a92176b134
Luca <47627900+Lucaa8@users.noreply.github.com>
2023-11-16 20:34:19 +0100

    Changed flask port

--- server/server.py
+++ server/server.py
@@ -48,4 +48,4 @@ def get_resource(filename):
     return send_from_directory(app.config['PUBLIC_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=8080)
